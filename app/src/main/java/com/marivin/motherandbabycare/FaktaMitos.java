b8058e89bbe6904e20de46557a4b1b631aabbf6a
taufiqtab <taufiqtab77@gmail.com>
2019-05-14 11:23:21 +0700

    All Screen Activity

---
+++ app/src/main/java/com/marivin/motherandbabycare/FaktaMitos.java
@@ -0,0 +1,13 @@
+package com.marivin.motherandbabycare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaktaMitos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fakta_mitos);
+    }
+}
---
+++ app/src/main/java/com/marivin/motherandbabycare/MotherCare.java
@@ -0,0 +1,13 @@
+package com.marivin.motherandbabycare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MotherCare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mother_care);
+    }
+}
